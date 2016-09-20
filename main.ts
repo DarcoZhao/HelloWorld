2dc8d1a0bb48f1c302e0381d3be293c53f433900
apple-PC\apple <562019412@qq.com>
2016-09-20 19:42:20 +0800

    ```Finish Homework2```

--- main.ts
+++ main.ts
@@ -1,17 +1,22 @@
 var x:string = process.argv[2];
 var num:number = parseInt(x);
+Tree(num);
 
-var line, b, c:number;
-var star, space:string;
 
-for(line=0; line<num; line++){
-    star = "";
-    space = "";
-    for(b=0; b<(num-line-1); b++){
-        space = space + " ";
+function Tree (x:number){
+    var line, spaNum, starNum:number;
+    var star, space:string;
+
+    for(line=0; line<num; line++){
+        star = "";
+        space = "";
+        for(spaNum=0; spaNum<(num-line-1); spaNum++){
+            space = space + " ";
+        }
+        for(starNum=0; starNum<(2*line+1); starNum++){
+            star = star + "*";
+        }
+        console.log(space + star);
     }
-    for(c=0; c<(2*line+1); c++){
-        star = star + "*";
-    }
-    console.log(space + star);
 }
+
